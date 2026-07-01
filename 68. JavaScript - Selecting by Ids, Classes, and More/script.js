60730856793657883a968ba03bad4eea2bc029e9
Tarun saini <tarunsainisaini661@gmail.com>
2026-07-02 01:03:09 +0530

    Add files via upload

---
+++ 68. JavaScript - Selecting by Ids, Classes, and More/script.js
@@ -0,0 +1,16 @@
+console.log("Tarun Saini")
+
+// let boxes = document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+
+// document.getElementById("redbox").style.backgroundColor = "red"
+
+// document.querySelector(".box").style.backgroundColor = "green";
+console.log(document.querySelectorAll(".box"))
+
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = "green";
+})
---
+++ 73. JavaScript Exercise 13 - Dynamic Website Builder/script.js
@@ -0,0 +1,7 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+    // Finish this function
+}
+
+
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithTarun", "560k", 7, "31:22", "https://i.ytimg.com/vi/KtL-SQ20Q0s/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLABJhdIBqlilwXKzJqa6StynbJT4Q")
